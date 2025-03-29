8ae82a947fd3ea2dc1dfb1a0160944ffaf4568b1
rdgeorge2 <104026438+rdgeorge2@users.noreply.github.com>
2025-03-28 22:51:30 -0300

    Atulizando Projeto

---
+++ projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/TaskControllerImpl.java
@@ -0,0 +1,9 @@
+package br.com.ada.t1322.tecnicasprogramacao.projeto;
+
+import br.com.ada.t1322.tecnicasprogramacao.projeto.controller.TaskController;
+import br.com.ada.t1322.tecnicasprogramacao.projeto.service.TaskService;
+
+public class TaskControllerImpl implements TaskController {
+    public TaskControllerImpl(TaskService taskService) {
+    }
+}
